897e95bc9214ac6f549ff60a10b08224a0c2e843
Mustafa Kaan Güngör <kaan062004+github@gmail.com>
2024-08-24 21:42:34 +0300

    added state control into player

--- Assets/_Scripts/Player.cs
+++ Assets/_Scripts/Player.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
@@ -8,8 +9,11 @@ public class Player : MonoBehaviour
     [SerializeField] int damage;
     [SerializeField] Enemy rival;
 
+    [SerializeField] Button attackButton;
+
     private void Awake() {
         health = GetComponent<Health>();
+        GameManager.OnStateChanged += GameManagerOnStateChanged;
     }
     void Start()
     {
@@ -23,5 +27,24 @@ public class Player : MonoBehaviour
 
     public void AttackOpponent() {
         rival.GetComponent<Health>().Damage(damage);
+    }
+
+    private void GameManagerOnStateChanged(GameState state) {
+        switch(state) {
+            case GameState.GameStart:
+            break;
+            case GameState.PlayerTurn:
+            break;
+            case GameState.PlayerAction:
+            break;
+            case GameState.OpponentTurn:
+            break;
+            case GameState.OpoonentAction:
+            break;
+            case GameState.Victory:
+            break;
+            case GameState.Lose:
+            break;
+        }
     }
 }
